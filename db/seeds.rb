61bf6dd29b41577604cf79d607fa807b82ace928
june29 <june29.jp@gmail.com>
2011-10-30 17:16:54 +0900

    prepare Event data as seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,35 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+# -*- coding: utf-8 -*-
+
+Event.create(:serial => '001tokyo', :name => '第1回 高専カンファレンス', :turnout => 30, :place => 'ウノウ株式会社 Unoh Inc.', :address => '東京都渋谷区渋谷3-13-11渋谷TKビル8', :date => '2008-06-14 00:00:00 UTC')
+Event.create(:serial => '002hokkaido', :name => '高専カンファレンス in 北海道', :turnout => 25, :place => '札幌市産業振興センター セミナールーム3', :address => '札幌市白石区東札幌5条1丁目1－1', :date => '2008-09-13 00:00:00 UTC')
+Event.create(:serial => '003tokyo', :name => '高専カンファレンス2008 Winter in 東京', :turnout => 55, :place => 'ミラクル・リナックス', :address => '東京都港区東新橋二丁目4番1号 サンマリーノ汐留5階', :date => '2008-12-07 00:00:00 UTC')
+Event.create(:serial => '028mie', :name => '高専カンファレンス in 三重', :turnout => 65, :place => '三重大学工学部17番教室', :address => '三重県津市栗真町屋町1577', :date => '2011-03-26 00:00:00 UTC')
+Event.create(:serial => '004fukui', :name => '高専カンファレンス in 福井', :turnout => 60, :place => '福井高専', :address => '福井県鯖江市下司町', :date => '2009-02-28 00:00:00 UTC')
+Event.create(:serial => '005kyushu', :name => '高専カンファレンス in 九州', :turnout => 65, :place => '久留米工業高等専門学校', :address => '福岡県久留米市小森野1丁目1-1', :date => '2009-05-16 00:00:00 UTC')
+Event.create(:serial => '006tokyo', :name => '東京 第3回 LT', :turnout => 20, :place => '株式会社オプティム', :address => '東京都港区芝 5-27-1 三田SSビル 3F', :date => '2009-04-04 00:00:00 UTC')
+Event.create(:serial => '008tohoku', :name => '高専カンファレンス in 東北', :turnout => 40, :place => '福島工業高等専門学校', :address => '福島県いわき市平上荒川字長尾30', :date => '2009-08-29 00:00:00 UTC')
+Event.create(:serial => '009tokyo', :name => '高専カンファレンス 2009秋 in 東京', :turnout => 200, :place => '東京都立産業技術高等専門学校 荒川キャンパス', :address => '東京都荒川区南千住8丁目17-1', :date => '2009-11-07 00:00:00 UTC')
+Event.create(:serial => '011nagano', :name => '高専カンファレンス in 長野', :turnout => 100, :place => '長野工業高等専門学校', :address => '長野県長野市徳間716', :date => '2009-12-19 00:00:00 UTC')
+Event.create(:serial => '012hachinohe', :name => '高専カンファレンス in 八戸', :turnout => 30, :place => '八戸工業高等専門学校', :address => '青森県八戸市大字田面木字上野平16-1', :date => '2010-01-30 00:00:00 UTC')
+Event.create(:serial => '013salesio', :name => '高専カンファレンス in サレジオ', :turnout => 200, :place => 'サレジオ工業高等専門学校', :address => '東京都町田市小山ヶ丘4-6-8', :date => '2010-04-24 00:00:00 UTC')
+Event.create(:serial => '010hokkaido', :name => '高専カンファレンス in 北海道2', :turnout => 50, :place => '苫小牧工業高等専門学校', :address => '北海道苫小牧市字錦岡443', :date => '2010-05-29 00:00:00 UTC')
+Event.create(:serial => '015nara', :name => '高専カンファレンス in 奈良', :turnout => 80, :place => '奈良工業高等専門学校', :address => '奈良県大和郡山市矢田町22', :date => '2010-07-17 00:00:00 UTC')
+Event.create(:serial => '017ishikawa', :name => '高専カンファレンス in 石川', :turnout => 60, :place => '石川工業高等専門学校', :address => '石川県河北郡津幡町字北中条タ-1', :date => '2010-08-07 00:00:00 UTC')
+Event.create(:serial => '018joshi-tokyo', :name => '高専女子カンファレンス in 東京', :turnout => 17, :place => '喫茶室ルノアール新宿区役所横店', :address => '東京都新宿区歌舞伎町1-3-5相模ビル2階', :date => '2010-09-25 00:00:00 UTC')
+Event.create(:serial => '016oita', :name => '高専カンファレンス in 九州～大分', :turnout => 0, :place => '大分工業高等専門学校', :address => '大分県大分市牧1666', :date => '2010-09-25 00:00:00 UTC')
+Event.create(:serial => '014tokyo', :name => '高専カンファレンス 2010秋 in 東京', :turnout => 200, :place => '東京都立産業技術高等専門学校 荒川キャンパス', :address => '東京都荒川区南千住8-17-1', :date => '2010-10-02 00:00:00 UTC')
+Event.create(:serial => '019kyoto', :name => '高専カンファレンス in 京都', :turnout => 64, :place => '京都高度技術研究所', :address => '京都府京都市下京区中堂寺南町134', :date => '2010-11-13 00:00:00 UTC')
+Event.create(:serial => '021numazu', :name => '高専カンファレンス in 沼津', :turnout => 64, :place => '沼津高専 図書館', :address => '静岡県沼津市大岡3600', :date => '2010-12-18 00:00:00 UTC')
+Event.create(:serial => '023shikoku', :name => '高専カンファレンス in 四国', :turnout => 19, :place => '新居浜工業高等専門学校', :address => '愛媛県新居浜市八雲町7-1', :date => '2011-01-22 00:00:00 UTC')
+Event.create(:serial => '024north-tohoku', :name => '高専カンファレンス in 北東北', :turnout => 17, :place => 'いわて県民情報交流センター', :address => '岩手県盛岡市盛岡駅西通1丁目7-1', :date => '2011-01-22 00:00:00 UTC')
+Event.create(:serial => '020salesio', :name => '高専カンファレンス in サレジオ2', :turnout => 120, :place => 'サレジオ工業高等専門学校 サレジアンホール', :address => '東京都町田市小山ヶ丘4丁目6-8', :date => '2011-02-12 00:00:00 UTC')
+Event.create(:serial => '027hokurikurb', :name => 'Hokuriku.rb x 高専カンファレンス', :turnout => 62, :place => '石川工業高等専門学校', :address => '石川県河北郡津幡町字北中条タ-1', :date => '2011-03-20 00:00:00 UTC')
+Event.create(:serial => '033careers', :name => '高専カンファレンス for careers', :turnout => 10, :place => '久留米工業高等専門学校 記念館 会議室', :address => '福岡県久留米市小森野1丁目1-1', :date => '2011-06-12 00:00:00 UTC')
+Event.create(:serial => '030joshi-tokyo', :name => '高専女子カンファレンス2', :turnout => 19, :place => 'チームラボ株式会社 TEAMLAB★INC', :address => '東京都文京区本郷4-9-22 本郷フジビル2F', :date => '2011-06-25 00:00:00 UTC')
+Event.create(:serial => '034nagano', :name => '高専カンファレンス in 長野2', :turnout => 126, :place => '長野工業高等専門学校', :address => '長野県長野市徳間716', :date => '2011-07-16 00:00:00 UTC')
+Event.create(:serial => '026osaka', :name => '高専カンファレンス in 大阪', :turnout => 61, :place => '大阪府立大学工業高等専門学校', :address => '大阪府寝屋川市幸町26-12', :date => '2011-08-27 00:00:00 UTC')
+Event.create(:serial => '022kobe', :name => '高専カンファレンス in 神戸', :turnout => 43, :place => '大学共同利用施設UNITY 第3セミナー室', :address => '神戸市西区学園西町1丁目1-1 ユニバープラザ2F', :date => '2011-01-08 00:00:00 UTC')
+Event.create(:serial => '037matsue', :name => '高専カンファレンス in 松江', :turnout => 60, :place => '松江テルサ', :address => '松江市朝日町478-18', :date => '2011-09-24 00:00:00 UTC')
+Event.create(:serial => '039oyama', :name => '高専カンファレンス in 小山', :turnout => 53, :place => '小山工業高等専門学校サテライトキャンパス・とちぎ蔵の街観光館', :address => '栃木市万町4-1', :date => '2011-09-25 00:00:00 UTC')
+Event.create(:serial => '025spa', :name => '高専カンファレンス in 鉱泉', :turnout => 0, :place => '伊東温泉', :address => '', :date => '2011-01-22 00:00:00 UTC')
+Event.create(:serial => '031sciences', :name => 'つくば理学カンファレンス', :turnout => 35, :place => '筑波大学3A203教室', :address => '茨城県つくば市天王台1丁目1-1', :date => '2011-10-08 00:00:00 UTC')
